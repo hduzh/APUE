78480c402d75dbcbe9c3c18dab6920b3f6e583a7
hduzh <80113845@qq.com>
2022-08-21 03:10:10 +0000

    #0 fix welcome.c error

--- apue-code/01/welcome.c
+++ apue-code/01/welcome.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
+
+///
+/// need add include file unistd.h, otherwise the program will core dump
 
 int
-main(int argc, char **argv) {
-	printf("Welcome to CS631 Advanced Programming in the UNIX Environment, %s!\n", getlogin())
+main() {
+	printf("Welcome to CS631 Advanced Programming in the UNIX Environment, %s!\n", getlogin());
 }
